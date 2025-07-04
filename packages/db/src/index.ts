cc8815238067e73644f1e3beab6ad342c12f976d
Rajesh sharma <110337036+XIN2025@users.noreply.github.com>
2025-07-04 17:50:44 +0530

    fix: remove build artifacts

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,4 +1,4 @@
-import * as Client from './generated/prisma';
+import * as Client from './generated/client';
 
 export const UserRole = Client.UserRole;
 export const ClientStatus = Client.ClientStatus;
@@ -12,7 +12,7 @@ export const ResourceType = Client.ResourceType;
 export const PrismaClient = Client.PrismaClient;
 export const Prisma = Client.Prisma;
 
-export * from './generated/prisma';
+export * from './generated/client';
 
 const prisma = new PrismaClient();
 export default prisma;
